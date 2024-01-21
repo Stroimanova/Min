8922ce488af8ced35a4957bc4659c9ef30ee97b5
unknown <simonastroimanova@hotmail.com>
2024-01-21 23:03:41 +0100

    first java program

---
+++ src/minjk/Min.java
@@ -0,0 +1,31 @@
+package minjk;
+
+
+public class Min {
+	 int min;
+
+	    void minFun(int n1, int n2) { // metod - min od dva broja
+	        min = n1;                // pretpostaven minimum
+	        if (min > n2) {
+	            min = n2;
+	        }
+	        System.out.println("Od broevite " + n1 + " i " + n2 + " pomal e " + min);
+	    }
+
+	    void minFun(int n1, int n2, int n3) { // metod - min od tri broja
+	        min = n1;                        // pretpostaven minimum
+	        if (min > n2) {
+	            min = n2;
+	        }
+	        if (min > n3) {
+	            min = n3;
+	        }
+	        System.out.println("Od broevite " + n1 + " " + n2 + " " + n3 + " najmal e " + min);
+	    }
+
+	    public static void main(String[] args) {
+	        Min najmal = new Min(); // kreiraj objekt
+	        najmal.minFun(11, 6); // povik na metod - min od dva broja
+	        najmal.minFun(6, 3, 4); // povik na metod - min od tri broja
+	    }
+}
